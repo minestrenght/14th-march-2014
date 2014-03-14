5572ceb36068e951122af4389a5ab112571e22fc
minestrenght <you@example.com>
2014-03-14 10:29:38 +0530

    synchronization demo added; why - will explain why its needed; how - will make it work

---
+++ synchronized/why/codes/Demo.java
@@ -0,0 +1,25 @@
+import static java.lang.System.out;
+class Demo {
+    public static void main(String[] args) {
+        Toilet t = new Toilet();
+        new Person("Adam", t).start();
+        new Person("Johny", t).start();
+    }
+}
+class Toilet {
+    public void use() {
+        out.printf("%s started using toilet\n", Thread.currentThread().getName());
+        out.printf("%s finished using toilet\n", Thread.currentThread().getName());
+    }
+}
+class Person extends Thread{
+    Toilet toilet;
+    Person(String name, Toilet t) {
+        super(name);
+        toilet = t;
+    }
+    @Override
+    public void run() {
+        toilet.use();
+    }
+}
